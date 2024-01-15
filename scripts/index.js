798376c6d075054e643e17b3e921061aae956084
Soni <soni@recalstudios.net>
2024-01-15 22:24:54 +0100

    Implement project page buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,22 @@
-$('#projects-content').load('/components/projects.html');
+// Load projects on load
+focusProjects();
+
+function focusProjects()
+{
+    // Modify UI
+    document.querySelector('#projects-page-button').classList.add('selected-page');
+    document.querySelector('#photos-page-button').classList.remove('selected-page');
+
+    // Load content
+    $('#projects-content').load('/components/projects.html');
+}
+
+function focusPhotos()
+{
+    // Modify UI
+    document.querySelector('#projects-page-button').classList.remove('selected-page');
+    document.querySelector('#photos-page-button').classList.add('selected-page');
+
+    // Load content
+    $('#projects-content').load('/components/photos.html');
+}
